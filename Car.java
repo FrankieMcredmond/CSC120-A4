6ca9d3d25266dcbd027071aa3661939d7578dd99
Frankie <emcredmond@smith.edu>
2024-09-30 18:16:29 -0400

    Car pt. 2

--- Car.java
+++ Car.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 public class Car {
     int max_capacity;
-    ArrayList<String> manifest;
+    ArrayList<Passenger> manifest;
 
     public Car(int max_capacity){
         this.max_capacity=max_capacity;
@@ -16,7 +16,29 @@ public class Car {
     public boolean addPassenger(Passenger p){
         if (max_capacity-getCapacity()>0){
             manifest.add(p);
-            if manifest.contain(p){return true
-        }
+            if (manifest.contains(p)){
+                return true;}
+            else{
+                return false;}}
+        else{
+            return false;}  }
+
+    public boolean removePassenger(String){
+        if (manifest.contains(string)){
+            manifest.remove(string);
+            return true;}
+        else{
+            return false;}}
+    public void printmanifest(){
+        if (manifest.size()==0){
+            System.out.println("sorry Car is empty");}
+        else{
+            for (int i = 0; i < max_capacity; i++){
+            System.out.println(manifest.get(i));}}}
+    public static void main(){
+        Car myCar= new Car(10);
+        myCar.addPassenger("Sarah");
+
     }
+    
 }
