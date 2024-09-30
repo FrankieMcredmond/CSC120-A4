9243ba4d23c3b9c38d08b09f37221559cabf8b8e
Frankie <emcredmond@smith.edu>
2024-09-30 14:58:24 -0400

    Engine and car pt. 1

--- Car.java
+++ Car.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
 public class Car {
+    int max_capacity;
+    ArrayList<String> manifest;
 
+    public Car(int max_capacity){
+        this.max_capacity=max_capacity;
+        this.manifest= new ArrayList<String>(max_capacity);
+    }
+    public int getCapacity(){
+        return manifest.size();
+    }
+    public int seatsRemaining(){
+        return max_capacity-getCapacity();
+    }
+    public boolean addPassenger(Passenger p){
+        if (max_capacity-getCapacity()>0){
+            manifest.add(p);
+            if manifest.contain(p){return true
+        }
+    }
 }
--- Engine.java
+++ Engine.java
@@ -1,3 +1,29 @@
 public class Engine {
+    FuelType powerType;
+    double current_fuel;
+    double max_fuel;
 
+
+    public Engine(FuelType powerType, double max_fuel ){
+        this.powerType=powerType;
+        this.current_fuel= 100.0;
+        this.max_fuel= max_fuel;
+    }
+    public void refuel(double current_fuel, double max_fuel){
+        current_fuel=max_fuel;
+    }
+    public boolean go(){
+        current_fuel-=5;
+        if (current_fuel>0){
+            return true;}
+        else {
+            return false;}
+    }
+
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
+        while (myEngine.go()) {
+            System.out.println("Choo choo!");
+        }
+        System.out.println("Out of fuel.");}
 }
